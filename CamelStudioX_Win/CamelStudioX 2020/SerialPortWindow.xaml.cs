5b6d2ef06c8adf731fdfda2dab575dbd4de3dca6
alien-wxr <49577496+alien-wxr@users.noreply.github.com>
2019-12-19 18:53:34 +0800

    optimize serialport window view

--- CamelStudioX_Win/CamelStudioX 2020/SerialPortWindow.xaml.cs
+++ CamelStudioX_Win/CamelStudioX 2020/SerialPortWindow.xaml.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MahApps.Metro.Controls;
+using System;
+using System.IO.Ports;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using MahApps.Metro.Controls;
-using System.IO.Ports;
 
 namespace CamelStudioX_2020
 {
