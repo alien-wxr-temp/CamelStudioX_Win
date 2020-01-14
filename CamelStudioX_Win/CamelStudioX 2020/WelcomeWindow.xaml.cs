f6db74ddbf5bc35138a1b5056f7ce23f3335b56d
alien-wxr <49577496+alien-wxr@users.noreply.github.com>
2020-01-14 16:52:59 +0800

    add ui for createNewProject function

--- CamelStudioX_Win/CamelStudioX 2020/WelcomeWindow.xaml.cs
+++ CamelStudioX_Win/CamelStudioX 2020/WelcomeWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using MahApps.Metro.Controls;
 using System.Windows;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace CamelStudioX_2020
 {
@@ -21,9 +23,31 @@ namespace CamelStudioX_2020
 
         private void newProject_Click(object sender, RoutedEventArgs e)
         {
+            this.tabControl1.SelectedIndex = 1;
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.pName.Text = "";
+            this.pLocation.Text = "";
+            this.tabControl1.SelectedIndex = 0;
+        }
+
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
             this.Close();
+        }
+
+        private void openButton_Click(object sender, RoutedEventArgs e)
+        {   
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;//设置为选择文件夹
+            if(dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                this.pLocation.Text = dialog.FileName;
+            }
         }
     }
 }
