90105c59fec3729290622d04e701211fee314b04
alien-wxr <49577496+alien-wxr@users.noreply.github.com>
2020-01-17 15:02:20 +0800

    add an example project

---
+++ CamelStudioX_Win/CamelStudioX 2020/example/M2/Hello/Hello.c
@@ -0,0 +1,11 @@
+﻿#include "stdio.h"
+
+//	This function is the user interrupt handler.
+void user_interrupt(void){}
+
+//This function is the main function
+int main(){
+	while(1){
+		puts("Hello world!\n");
+	}
+}
